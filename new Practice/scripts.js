f3ef4fe31e567cc7ddd63a4aeb73a5539ae0f30c
Oluwadamilare2003 <oluwadamilareobalana@gmail.com>
2022-11-03 18:02:26 +0100

    Update

--- new Practice/scripts.js
+++ new Practice/scripts.js
@@ -1,56 +1,82 @@
-const cards = document.querySelectorAll(".card");
-const observer = new IntersectionObserver(entries=>{
-    entries.forEach(entry=>{
-        entry.target.classList.toggle("show",entry.isIntersecting);
-        // if (entry.isIntersecting){observer.unobserve(entry.target)}
+// Display the hamburger Menu
+const hamburger = document.querySelector("#hamburger");
+const navigation = document.querySelector("#nav");
+const hamburgerOnClick = () => {
+    hamburger.classList.toggle("display");
+    navigation.classList.toggle("show1");
+}
+hamburger.addEventListener("click",hamburgerOnClick)
+
+
+// Lazy load the images
+const allImages = document.querySelectorAll("img[data-src]");
+
+const conversion = (images) =>{
+    images.setAttribute("src",images.getAttribute("data-src"));
+    images.onload=()=>{
+        images.removeAttribute("data-src")
+    }
+}
+
+const imageObserver = new IntersectionObserver((items,observer)=>{
+    items.forEach(item=>{
+        if(item.isIntersecting){
+            conversion(item.target);
+            observer.unobserve(item.target);
+        }
     })
-},{
-    threshold:.5,
-})
-cards.forEach(card=>{
-    observer.observe(card)
 })
 
+allImages.forEach(image=>{
+    imageObserver.observe(image)
+})
+
+
+// Slide the images into view
+const container = document.querySelectorAll(".container");
+const conatainerObserver = new IntersectionObserver(items=>{
+    items.forEach(item=>{
+        item.target.classList.toggle("conShow",item.isIntersecting);
+    })
+},{
+    threshold:.2,
+})
+container.forEach(con=>{
+    conatainerObserver.observe(con);
+})
+
+
+// Display the card containers
+const cards = document.querySelectorAll(".card");
+const cardsObserver = new IntersectionObserver(items=>{
+    items.forEach(item=>{
+        item.target.classList.toggle("show",item.isIntersecting)
+    })
+},{
+    threshold:1,
+})
+cards.forEach(card=>{
+    cardsObserver.observe(card);
+})
+
+// Display number of times visited
 const visited = document.querySelector(".visited");
-const divObserver = new IntersectionObserver(items=>{
+const visitObserver = new IntersectionObserver(items=>{
     items.forEach(item=>{
         item.target.classList.toggle("display",item.isIntersecting)
     })
 },{
-    threshold:.5,
+    threshold:.2,
 })
-divObserver.observe(visited);
+visitObserver.observe(visited)
 
-let timesVisited = localStorage.getItem("neVisit");
-const valueVisit = document.querySelector("#timesVisited");
-if(timesVisited > 0){
-    valueVisit.textContent = timesVisited;
+// Show the number of times visited
+const timesVisisted = document.querySelector("#timesVisited");
+let value1 = Number(localStorage.getItem("value"));
+if(value1>0){
+    timesVisisted.textContent = value1;
 }else{
-    valueVisit.textContent = 0;
+    timesVisisted.textContent = 0
 }
-timesVisited++
-localStorage.setItem("neVisit",timesVisited)
-
-const hamburger = document.querySelector("#hamburger");
-const navigation = document.querySelector("#nav")
-const displayEvent=()=>{
-    navigation.classList.toggle("show1");
-    hamburger.classList.toggle("display");
-}
-hamburger.addEventListener("click",displayEvent)
-
-const newImages = document.querySelectorAll(".image[data-src]");
-
-const imageObserver = new IntersectionObserver((callback)=>{
-    callback.forEach(image=>{
-        image.target.setAttribute("src",image.target.getAttribute("data-src"));
-        image.target.onload=()=>{
-            image.target.removeAttribute("data-src");
-        }
-        if(image.isIntersecting){imageObserver.unobserve(image.target)}
-    })
-})
-
-newImages.forEach(image=>{
-    imageObserver.observe(image)
-})
+value1++;
+localStorage.setItem("value",value1);
